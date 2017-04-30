6eb1bc56915fc4fb16940d7dd69fee2882c09ee1
John Stepan <jstep84@gmail.com>
2017-04-29 23:44:14 -0700

    first commit

---
+++ client/script.js
@@ -0,0 +1,5 @@
+$(function() {
+  $('input').on('click', function() {
+    $(this).parent().toggleClass('checked');
+  });
+});
---
+++ routes/todos/index.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+var Todo = require('../../models/todos');
+
+router.get('/', function(req, res, next) {
+  Todo.findAsync()
+  .then(function(todos) {
+    res.render('todos', {title: 'Todos', todos: todos});
+    console.log('workssss');
+  })
+  .catch(next)
+  .error(console.error);
+});
+
+
+module.exports = router;
